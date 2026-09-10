c5cb3b75f160e058767927e0de51291252c08a5d
andrewcole155-eng <andrewcole155@gmail.com>
2026-09-10 11:02:48 +1000

    Add files via upload

--- Mission_Control.py
+++ Mission_Control.py
@@ -1713,7 +1713,7 @@ with tab3:
                     "VERDICT_30D": st.column_config.TextColumn("30D Assessment", width="small"),
                     "PRIORITY": st.column_config.TextColumn("Priority", width="small"),
                 },
-                height=480
+                height=580
             )
 
         st.divider()
